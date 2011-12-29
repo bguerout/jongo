50a4cd028302ac7e1c4351536c946e5ef56f61ed
yvesamsellem <amsellem.yves@gmail.com>
2011-12-29 12:00:12 +0100

    get mongo _id with @Id on a String property

---
+++ src/main/java/com/jongo/MongoPropertyNamingStrategy.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2011 Benoit GUEROUT <bguerout at gmail dot com> and Yves AMSELLEM <amsellem dot yves at gmail dot com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jongo;
+
+import javax.persistence.Id;
+
+import org.codehaus.jackson.map.MapperConfig;
+import org.codehaus.jackson.map.PropertyNamingStrategy;
+import org.codehaus.jackson.map.introspect.AnnotatedField;
+
+public class MongoPropertyNamingStrategy extends PropertyNamingStrategy {
+    @Override
+    public String nameForField(MapperConfig<?> config, AnnotatedField field, String defaultName) {
+	if (field.getAnnotated().isAnnotationPresent(Id.class))
+	    return "_id";
+	return super.nameForField(config, field, defaultName);
+    }
+}
