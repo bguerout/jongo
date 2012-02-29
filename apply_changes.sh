fe68cbb9a2e358d7935cf47c3fff2692dfba4711
Benoit Guerout <bguerout@gmail.com>
2012-02-29 11:41:11 +0100

    Remove unused script

--- apply_changes.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
- echo This script must on gh-pages branch 
- git rm -r --ignore-unmatch * 
- mv gh-pages/* ./ 
- rm -rf gh-pages/ 
- rm /scratch/hudson/workspace/release-site/jongo/apply_changes.sh
- git add .
