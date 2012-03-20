a47e01a54e97b574bc87474d0b8eec5423d05136
Benoit Guerout <bguerout@gmail.com>
2012-03-20 21:29:23 +0100

    Fix static dirname in server.js

--- scripts/server.js
+++ scripts/server.js
@@ -18,8 +18,8 @@ var express = require('express');
 var app = express.createServer();
 app.configure(
     function () {
-        app.use(express.logger({ format: ':method :url' }));
-        app.use(express.static(__dirname));
+        app.use(express.logger({ format:':method :url' }));
+        app.use(express.static(__dirname + '/..'));
         app.use(express.errorHandler({ dumpExceptions:true, showStack:true }));
     }).listen(3000);
 
