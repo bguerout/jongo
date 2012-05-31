1def7f938409b51d0d11efe52d2440f8f2d114ca
Benoit Guerout <bguerout@gmail.com>
2012-05-31 16:56:10 +0200

    Refactor query tests

---
+++ src/test/java/org/jongo/query/ParameterizedQueryTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2011 Benoit GUEROUT <bguerout at gmail dot com> and Yves AMSELLEM <amsellem dot yves at gmail dot com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jongo.query;
+
+import com.mongodb.DBObject;
+import org.jongo.query.ParameterBinder;
+import org.jongo.query.ParameterizedQuery;
+import org.jongo.query.Query;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+public class ParameterizedQueryTest {
+
+    private ParameterBinder binder;
+
+    @Before
+    public void setUp() throws Exception {
+        binder = mock(ParameterBinder.class);
+    }
+
+    @Test
+    public void whenQueryHasNoParameterShouldConvertToDBObject() throws Exception {
+
+        Query query = new ParameterizedQuery(binder, "{'value':1}");
+
+        DBObject dbObject = query.toDBObject();
+
+        assertThat(dbObject.containsField("value")).isTrue();
+        assertThat(dbObject.get("value")).isEqualTo(1);
+        verify(binder, never()).bind(anyString(), anyVararg());
+    }
+
+    @Test
+    public void whenQueryHasParametersShouldBindParamsAndConvertToDBObject() throws Exception {
+
+        Query query = new ParameterizedQuery(binder, "{'value':#}", "2");
+        when(binder.bind("{'value':#}", "2")).thenReturn("{'value':2}");
+
+        DBObject dbObject = query.toDBObject();
+
+        verify(binder).bind("{'value':#}", "2");
+        assertThat(dbObject.containsField("value")).isTrue();
+        assertThat(dbObject.get("value")).isEqualTo(2);
+    }
+
+}
--- src/test/java/org/jongo/query/StaticQueryTest.java
+++ src/test/java/org/jongo/query/StaticQueryTest.java
@@ -17,47 +17,22 @@
 package org.jongo.query;
 
 import com.mongodb.DBObject;
-import org.jongo.query.ParameterBinder;
-import org.jongo.query.ParameterizedQuery;
-import org.jongo.query.Query;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.fest.assertions.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
-public class QueryTest {
-
-    private ParameterBinder binder;
-
-    @Before
-    public void setUp() throws Exception {
-        binder = mock(ParameterBinder.class);
-    }
+public class StaticQueryTest {
 
     @Test
     public void shouldConvertToDBObject() throws Exception {
 
-        Query query = new ParameterizedQuery(binder, "{'value':1}");
+        Query query = new StaticQuery("{'value':1}");
 
         DBObject dbObject = query.toDBObject();
 
         assertThat(dbObject.containsField("value")).isTrue();
         assertThat(dbObject.get("value")).isEqualTo(1);
-        verify(binder, never()).bind(anyString(), anyVararg());
     }
-
-    @Test
-    public void shouldBindParamsAndConvertToDBObject() throws Exception {
-
-        Query query = new ParameterizedQuery(binder, "{'value':#}", "2");
-        when(binder.bind("{'value':#}", "2")).thenReturn("{'value':2}");
-
-        DBObject dbObject = query.toDBObject();
-
-        verify(binder).bind("{'value':#}", "2");
-        assertThat(dbObject.containsField("value")).isTrue();
-        assertThat(dbObject.get("value")).isEqualTo(2);
-    }
-
 }
