054c625a241b42117a9f62aa3e881af531aaf0c1
Benoit Guerout <bguerout@gmail.com>
2012-06-28 19:35:23 +0200

    Allow publishing in dry mode

--- publish-site.sh
+++ publish-site.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
-echo This script must on gh-pages branch
+
+stop_on_error() {
+ if [ $? -ne 0 ] ; then
+  echo "$1"
+  exit 1
+ fi
+}
+
+echo "Running publish script with dry=$DRY_RUN and commit message=$COMMIT_MESSAGE"
 
 #Clean
 rm -rf jongo/ node*
 
 #Install node
 source /private/jongo/install-node.sh
-if [ $? -ne 0 ] ; then
- echo "Unable to install node"
- exit 1
-fi
+stop_on_error "Unable to install node"
 
 #Download node dependencies
 npm install express@2.5.8 jake@0.2.18 jsdom@0.2.12 less@1.2.2 wrench@1.3.7
+stop_on_error "Unable to install npm dependencies"
 export NODE_PATH=$PWD/node_modules
 
 #Clone repository and checkout site branch
@@ -24,11 +30,16 @@ git checkout site
 
 #Run jake on site branch
 node $NODE_PATH/jake/bin/cli.js -f scripts/generate-site.js
-if [ $? -ne 0 ] ; then
- echo "Jake script has failed"
- exit 1
-fi
+stop_on_error "Jake script has failed"
 
 #Trash current files and apply new sources
 git checkout -b gh-pages origin/gh-pages
-git rm -r --ignore-unmatch * && mv generated-site/* ./ && rm -rf generated-site/ && git add ./ && git status && git commit -m "$COMMIT_MESSAGE" && git push
+git rm -r --ignore-unmatch * && mv generated-site/* ./ && rm -rf generated-site/ && git add ./ && git status
+stop_on_error "Unable to apply sources from site branch on gh-pages branch"
+
+if [ "$DRY_RUN" = "false" ]; then
+ echo "Pushing site to gh-pages"
+ git commit -m "$COMMIT_MESSAGE" && git push
+else
+ echo "Script runs in DRY mode, nothing has been pushed." 
+fi
