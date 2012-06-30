8d4e50d2998647f81d0feeb3f623303f44900bfc
yamsellem <amsellem.yves@gmail.com>
2012-06-30 21:00:45 +0200

    release notes and minor fixes

--- assets/js/jongo.js
+++ assets/js/jongo.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-   prettyPrint();
+	prettyPrint();
+
+	$('a').click(function() {
+		$('html, body').animate({
+			scrollTop: $($(this).attr('href')).offset().top + 'px'
+		}, {
+			duration: 450,
+			easing: 'swing'
+		});
+		return false;
+	});
 });
