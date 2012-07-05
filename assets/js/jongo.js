941e326b1248ee6cec68b16237eafe9454d3bc12
CloudBees DEV@Cloud <nobody@cloudbees.com>
2012-07-05 10:36:19 -0400

    0.2 release - Publish Site

--- assets/js/jongo.js
+++ assets/js/jongo.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-   prettyPrint();
+	prettyPrint();
+	$('.nav-collapse').scrollspy();
+
+	$('a').click(function() {
+		$('html, body').animate({
+			scrollTop: $($(this).attr('href')).offset().top - 20 + 'px'
+		}, {
+			duration: 450,
+			easing: 'swing'
+		});
+		return false;
+	});
 });
