130ef70cb75aff5c59860a45fc957f1cbff9aba5
yamsellem <amsellem.yves@gmail.com>
2012-07-14 11:00:38 +0200

    refine scroll animation

--- assets/js/jongo.js
+++ assets/js/jongo.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
 	prettyPrint();
 
-	$('a').click(function() {
+	$('a').click(function(event) {
+		var href = $(event.target).attr('href');
+		var scroll = $($(this).attr('href')).offset().top;
+		if(href == '#overview')
+			scroll -= 30;
+
 		$('html, body').animate({
-			scrollTop: $($(this).attr('href')).offset().top - 20 + 'px'
+			scrollTop: scroll
 		}, {
 			duration: 450,
 			easing: 'swing'
