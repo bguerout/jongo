cb2ad74da6314aaa6cf16dec34b01da3a1eb8b9f
Benoit Guerout <bguerout@gmail.com>
2012-09-18 22:22:11 +0200

    Add 0.2 tags and update versions menu

---
+++ tags/0.2/assets/js/jongo.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	prettyPrint();
+
+	$('a').click(function() {
+		var href = $(this).attr('href');
+
+		var scroll = $(href).offset().top;
+		if(href == '#overview')
+			scroll -= 30;
+
+		$('html, body').animate({
+			scrollTop: scroll
+		}, {
+			duration: 450,
+			easing: 'swing'
+		});
+		return false;
+	});
+});
