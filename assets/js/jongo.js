c16a059ddbe7c24be35076d59d4cb60e67d40974
yamsellem <amsellem.yves@gmail.com>
2012-11-20 00:01:50 +0100

    Fix header on the welcoming screen bottom

--- assets/js/jongo.js
+++ assets/js/jongo.js
@@ -3,12 +3,6 @@ $(document).ready(function() {
 
   $('.nav-collapse').scrollspy();
 
-  $('.navbar').affix({
-    offset: {
-      top: function () { return $(window).width() > 750 ? 705 : 1036 }
-    }
-  });
-
   $('a').click(function() {
     var href = $(this).attr('href');
 
@@ -17,11 +11,23 @@ $(document).ready(function() {
       $('html, body').animate({
         scrollTop: offset.top
       }, 
-        'slow', 
-        'swing', 
+        'slow', 'swing', 
         function() { if(href === '#jongo') href = ''; location.hash = href; } 
       );
       return false;
+    }
+  });
+
+  var gap = 0, height = $(window).height();
+  if(height > 746 && height < 1146) {
+    gap = height - 746;
+    var bottom = gap / 2, $row = $('#overview .intro .row:last-child');
+    $row.css('margin-top', gap - bottom).css('margin-bottom', bottom + 10);
+  }
+
+  $('.navbar').affix({
+    offset: {
+      top: function () { return $(window).width() > 750 ? 705 + gap : 1036 + gap}
     }
   });
 });
