ac932b704fa08e1147011f3045a0793547da9ae5
yamsellem <amsellem.yves@gmail.com>
2012-11-22 23:53:45 +0100

    Fix header auto-positionning

--- assets/js/jongo.js
+++ assets/js/jongo.js
@@ -19,17 +19,20 @@ $(document).ready(function() {
   var gap = 0;
   var adjust = function() {
     var height = $(window).height(), width = $(window).width();
-    if(width > 750 && height > 746 && height < 1046) {
+    
+    if(width > 750 && height > 746) {
+      if(height > 945) height = 945;
       gap = height - 746;
-      var bottom = gap / 2, $row = $('#overview .intro .row:last-child');
-      $row.css('margin-top', gap - bottom).css('margin-bottom', bottom + 10);
+    } else {
+      gap = 0;
     }
+    
+    var bottom = gap / 2, $row = $('#overview .intro .row:last-child');
+    $row.css('margin-top', gap - bottom).css('margin-bottom', bottom + 10);
   }
 
   prettyPrint();
   $('.nav-collapse').scrollspy();
-  scrollInnerLinks();
-  
   adjust();
   $(window).resize(adjust);
   $('.navbar').affix({ offset: { top: function () { return $(window).width() > 750 ? 705 + gap : 1036 + gap }}});
