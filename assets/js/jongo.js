f108ab657fe56df3d23f331d9367e5c9847168d0
CloudBees DEV@Cloud <nobody@cloudbees.com>
2012-12-16 05:35:07 -0500

    Publish Site

--- assets/js/jongo.js
+++ assets/js/jongo.js
@@ -1,19 +1,24 @@
 $(document).ready(function() {
-	prettyPrint();
+  var gap = 0;
+  var adjust = function() {
+    var height = $(window).height(), width = $(window).width();
 
-	$('a').click(function() {
-		var href = $(this).attr('href');
+    if(width > 750 && height > 746) {
+      if(height > 900) height = 900;
+      gap = height - 746;
+    } else {
+      gap = 0;
+    }
+    
+    var bottom = gap / 2, $row = $('#overview .intro .row:last-child');
+    $row.css('margin-top', gap - bottom).css('margin-bottom', bottom + 10);
+  }
 
-		var scroll = $(href).offset().top;
-		if(href == '#overview')
-			scroll -= 30;
+  prettyPrint();
+  $('.nav-collapse').scrollspy();
+  $().smoothanchors();
 
-		$('html, body').animate({
-			scrollTop: scroll
-		}, {
-			duration: 450,
-			easing: 'swing'
-		});
-		return false;
-	});
+  adjust();
+  $(window).resize(adjust);
+  $('.navbar').affix({ offset: { top: function () { return $(window).width() > 750 ? 705 + gap : 1036 + gap }}});
 });
