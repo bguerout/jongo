38255aca841073dfe7e399e42c4f4aa4f90f59cb
Benoit Guerout <bguerout@gmail.com>
2012-12-21 23:40:05 +0100

    Install nodejs in target folder

--- scripts/install-node.sh
+++ scripts/install-node.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 NODE_DIST=node-v0.8.16-linux-x64
+NODE_INSTALL_DIR=target/$NODE_DIST
 NODE_BIN_FILE=$NODE_DIST.tar.gz
 
 stop_on_download_error() {
@@ -14,18 +15,18 @@ then
     echo "Installing $NODE_DIST"
     wget --no-verbose -O $NODE_BIN_FILE https://github.com/CloudBees-community/node-clickstack/blob/master/$NODE_BIN_FILE?raw=true
     stop_on_download_error "Unable to download $NODE_BIN_FILE"
-    tar xf $NODE_BIN_FILE
-    export PATH=$PWD/$NODE_DIST/bin:${PATH}
+    tar xf $NODE_BIN_FILE -C target
+    export PATH=$PWD/$NODE_INSTALL_DIR/bin:${PATH}
     echo "nodejs $(node -v) has been installed."
 
     echo "Installing npm"
-    pushd $NODE_DIST
+    pushd $NODE_INSTALL_DIR
     curl https://npmjs.org/install.sh | clean=yes sh
     stop_on_download_error "Unable to download npm"
     popd
     echo "npm $(npm -v) has been installed."
 else
-    export PATH=$PWD/$NODE_DIST/bin:${PATH}
+    export PATH=$PWD/$NODE_INSTALL_DIR/bin:${PATH}
 fi
 
 
