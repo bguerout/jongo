4161b829b3dfdaa2eb4e6a8d27a0cd23f3483d18
Benoit Guerout <bguerout@gmail.com>
2012-12-22 10:46:38 +0100

    Apply site changes on a dedicated working dir
    Before changes where applied on current working dir
    by checkouting gh-pages branch

--- publish-site.sh
+++ publish-site.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
 GENERATED_SITE_DIR=target/jongo_org_website
+GHPAGES_REPO_DIR=target/gh-pages
 
 stop_on_error() {
  if [ $? -ne 0 ] ; then
+  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
   echo "$1"
+  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
   exit 1
  fi
 }
 
+echo "***************************************************************************"
 echo "Running publish script with dry=$DRY_RUN and commit message=$COMMIT_MESSAGE"
+echo "***************************************************************************"
 
 #Install node
 source scripts/install-node-dependencies.sh
@@ -19,14 +24,21 @@ stop_on_error "Unable to install node or a dependency"
 node $PWD/node_modules/jake/bin/cli.js -f scripts/generate-site.js
 stop_on_error "Jake script has failed"
 
-#Trash current files and apply new sources
-git checkout gh-pages
+echo "******************************************"
+echo "Applying site branch changes into gh-pages"
+echo "******************************************"
+rm -rf $GHPAGES_REPO_DIR
+git clone --branch gh-pages git@github.com:bguerout/jongo.git $GHPAGES_REPO_DIR
+
+pushd $GHPAGES_REPO_DIR
 git rm -r --ignore-unmatch * && mv $GENERATED_SITE_DIR/* ./ && rm -rf $GENERATED_SITE_DIR && git add ./ && git status && git diff --staged
 stop_on_error "Unable to apply sources from site branch on gh-pages branch"
-
 if [ "$DRY_RUN" = "false" ]; then
+ echo "************************"
  echo "Pushing site to gh-pages"
+ echo "************************"
  git commit -m "$COMMIT_MESSAGE" && git push
 else
  echo "Script runs in DRY mode, nothing has been pushed." 
 fi
+popd
--- scripts/install-node-dependencies.sh
+++ scripts/install-node-dependencies.sh
@@ -6,13 +6,18 @@ NODE_BIN_FILE=$NODE_DIST.tar.gz
 
 stop_on_install_error() {
  if [ $? -ne 0 ] ; then
+  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
   echo "$1"
+  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
   exit 1
  fi
 }
 if [ -d $NODE_INSTALL_DIR ]
 then
     export PATH=$PWD/$NODE_INSTALL_DIR/bin:${PATH}
+    echo "******************************************************"
+    echo "nodejs is already available at $PWD/$NODE_INSTALL_DIR"
+    echo "******************************************************"
 else
     echo "Installing $NODE_DIST"
     wget --no-verbose -O $NODE_BIN_FILE https://github.com/CloudBees-community/node-clickstack/blob/master/$NODE_BIN_FILE?raw=true
@@ -21,18 +26,21 @@ else
     tar xf $NODE_BIN_FILE -C target
     rm $NODE_BIN_FILE
     export PATH=$PWD/$NODE_INSTALL_DIR/bin:${PATH}
+    echo "******************************************"
     echo "nodejs $(node -v) has been installed."
-    echo
+    echo "******************************************"
 
     echo "Installing npm"
     pushd $NODE_INSTALL_DIR
     curl https://npmjs.org/install.sh | clean=yes sh
     stop_on_install_error "Unable to download npm"
     popd
+    echo "******************************************"
     echo "npm $(npm -v) has been installed."
-    echo
+    echo "******************************************"
 
     #Download node dependencies
+    echo "Installing npm dependencies"
     npm install express@3.0.5 jake@0.5.8 jsdom@0.3.3 less@1.3.1 wrench@1.4.4
     stop_on_install_error "Unable to install npm dependencies"
 fi
