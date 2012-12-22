51f2a3bb1c6f5326915bdaff35294db41f8e5331
Benoit Guerout <bguerout@gmail.com>
2012-12-22 09:44:53 +0100

    Move npm deps downloading into dedicated script

--- publish-site.sh
+++ publish-site.sh
@@ -10,21 +10,16 @@ stop_on_error() {
 echo "Running publish script with dry=$DRY_RUN and commit message=$COMMIT_MESSAGE"
 
 #Install node
-source scripts/install-node.sh
-stop_on_error "Unable to install node"
-
-#Download node dependencies
-npm install express@3.0.5 jake@0.5.8 jsdom@0.3.3 less@1.3.1 wrench@1.4.4
-stop_on_error "Unable to install npm dependencies"
-export NODE_PATH=$PWD/node_modules
+source scripts/install-node-dependencies.sh
+stop_on_error "Unable to install node or a dependency"
 
 #Run jake on site branch
-node $NODE_PATH/jake/bin/cli.js -f scripts/generate-site.js
+node $PWD/node_modules/jake/bin/cli.js -f scripts/generate-site.js
 stop_on_error "Jake script has failed"
 
 #Trash current files and apply new sources
 git checkout gh-pages
-git rm -r --ignore-unmatch * && mv target/jongo_org_website/* ./ && rm -rf target/ && git add ./ && git status
+git rm -r --ignore-unmatch * && mv target/jongo_org_website/* ./ && rm -rf target/ && git add ./ && git status && git diff --staged
 stop_on_error "Unable to apply sources from site branch on gh-pages branch"
 
 if [ "$DRY_RUN" = "false" ]; then
--- scripts/install-node-dependencies.sh
+++ scripts/install-node-dependencies.sh
@@ -4,7 +4,7 @@ NODE_DIST=node-v0.8.16-linux-x64
 NODE_INSTALL_DIR=target/$NODE_DIST
 NODE_BIN_FILE=$NODE_DIST.tar.gz
 
-stop_on_download_error() {
+stop_on_install_error() {
  if [ $? -ne 0 ] ; then
   echo "$1"
   exit 1
@@ -16,7 +16,7 @@ then
 else
     echo "Installing $NODE_DIST"
     wget --no-verbose -O $NODE_BIN_FILE https://github.com/CloudBees-community/node-clickstack/blob/master/$NODE_BIN_FILE?raw=true
-    stop_on_download_error "Unable to download $NODE_BIN_FILE"
+    stop_on_install_error "Unable to download $NODE_BIN_FILE"
     mkdir target
     tar xf $NODE_BIN_FILE -C target
     rm $NODE_BIN_FILE
@@ -27,10 +27,14 @@ else
     echo "Installing npm"
     pushd $NODE_INSTALL_DIR
     curl https://npmjs.org/install.sh | clean=yes sh
-    stop_on_download_error "Unable to download npm"
+    stop_on_install_error "Unable to download npm"
     popd
     echo "npm $(npm -v) has been installed."
     echo
 fi
+
+#Download node dependencies
+npm install express@3.0.5 jake@0.5.8 jsdom@0.3.3 less@1.3.1 wrench@1.4.4
+stop_on_install_error "Unable to install npm dependencies"
 
 
