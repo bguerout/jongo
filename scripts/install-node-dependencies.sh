786d66a7d8febec34c4440c40eb2c48f4059ea15
Benoit Guerout <bguerout@gmail.com>
2013-02-17 17:04:18 +0100

    Replace custom nodejs install by NVM

--- scripts/install-node-dependencies.sh
+++ scripts/install-node-dependencies.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-NODE_VERSION=0.8.17
-NODE_INSTALL_DIR=target/node-v$NODE_VERSION-linux-x64
-NODE_BIN_FILE=node-$NODE_VERSION.tar.gz
+NODE_VERSION=0.8.20
 
 stop_on_install_error() {
  if [ $? -ne 0 ] ; then
@@ -12,37 +10,21 @@ stop_on_install_error() {
   exit 1
  fi
 }
-if [ -d $NODE_INSTALL_DIR ]
-then
-    export PATH=$PWD/$NODE_INSTALL_DIR/bin:${PATH}
-    echo "nodejs is already available at $PWD/$NODE_INSTALL_DIR"
-else
-    echo "Installing nodejs $NODE_VERSION"
-    wget --no-verbose -O $NODE_BIN_FILE http://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-x64.tar.gz
-    stop_on_install_error "Unable to download $NODE_BIN_FILE"
-    mkdir target
-    tar xf $NODE_BIN_FILE -C target
-    rm $NODE_BIN_FILE
-    export PATH=$PWD/$NODE_INSTALL_DIR/bin:${PATH}
-    node -v
-    stop_on_install_error "Unable to install node"
-    echo "*************************************"
-    echo "nodejs $(node -v) has been installed."
-    echo "*************************************"
 
-    echo "Installing npm"
-    pushd $NODE_INSTALL_DIR
-    curl https://npmjs.org/install.sh | clean=yes sh
-    stop_on_install_error "Unable to download npm"
-    popd
-    echo "*********************************"
-    echo "npm $(npm -v) has been installed."
-    echo "*********************************"
+echo "Installing nodejs $NODE_VERSION"
+curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+. "$HOME/.nvm/nvm.sh"
+stop_on_install_error "nvm has not been installed"
 
-    #Download node dependencies
-    echo "Installing npm dependencies"
-    npm install express@3.0.5 jake@0.5.8 jsdom@0.3.3 less@1.3.1 wrench@1.4.4
-    stop_on_install_error "Unable to install npm dependencies"
-fi
+nvm install $NODE_VERSION
+nvm use $NODE_VERSION
+node -v
+stop_on_install_error "node has not been installed"
+
+#Download node dependencies
+echo "Installing npm dependencies"
+npm install express@3.0.5 jake@0.5.8 jsdom@0.3.3 less@1.3.1 wrench@1.4.4
+stop_on_install_error "Unable to install npm dependencies"
+
 
 
