016c5c0acd1a4cf033003dde3918c0bf3b25f862
Benoit Guerout <bguerout@gmail.com>
2013-03-22 08:22:06 +0100

    Ensure all driver versions are tested

--- src/test/sh/run-tests-against-all-driver-versions.sh
+++ src/test/sh/run-tests-against-all-driver-versions.sh
@@ -3,20 +3,22 @@
 OUTPUT_DIR=./target/mongo-compatibility
 MONGO_ARTIFACTS_FILE=./target/mongo-versions
 NEXUS_URL="https://oss.sonatype.org/service/local/data_index?g=org.mongodb&a=mongo-java-driver"
-LAST_UNSUPPORTED_VERSION="2.8.0"
+MINIMAL_VERSION="2.8.0"
 A_VERSION_HAS_FAILED=false
 OPTS=$*
 
-echo "Executing tests with mongo-java-driver[$LAST_UNSUPPORTED_VERSION+] dependencies available on Nexus http://repository.sonatype.org"
+echo "Executing tests with mongo-java-driver[$MINIMAL_VERSION+] dependencies available on Nexus http://repository.sonatype.org"
 
 mkdir -p $OUTPUT_DIR;
-VERSIONS=`curl -so $MONGO_ARTIFACTS_FILE $NEXUS_URL &&  grep -e "version" $MONGO_ARTIFACTS_FILE | sed 's/<version>//g' | sed 's/<\/version>//g' | tr -s " " | sort -u`;
+VERSIONS=`curl -so $MONGO_ARTIFACTS_FILE $NEXUS_URL &&  grep -e "version" $MONGO_ARTIFACTS_FILE | sed 's/<version>//g' | sed 's/<\/version>//g' | tr -s " " | uniq`;
 
 for version in $VERSIONS
 do
-    if [ "$version" \> "$LAST_UNSUPPORTED_VERSION" ] || [ "$version" = "$LAST_UNSUPPORTED_VERSION" ];
+    IFS='.' read -ra current <<< "$version"
+    IFS='.' read -ra minimal <<< "$MINIMAL_VERSION"
+    if [ ${current[0]} -ge ${minimal[0]} ] && [ ${current[1]} -ge ${minimal[1]} ];
     then
-      mvn verify $OPTS -Dmongo.version=$version -DreportFormat=plain -DuseFile=false -l $OUTPUT_DIR/build-$version.log 
+      mvn verify $OPTS -Dmongo.version=$version -DreportFormat=plain -DuseFile=false -l $OUTPUT_DIR/build-$version.log
 
       if [ "$?" -ne "0" ];
       then
