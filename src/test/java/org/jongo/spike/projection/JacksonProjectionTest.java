fa725f412e9a84853836186b032f8adcc1f44d77
Benoit Guerout <bguerout@gmail.com>
2013-08-14 14:32:34 +0200

    Remove getMapper in Mapping class

--- src/test/java/org/jongo/spike/projection/JacksonProjectionTest.java
+++ src/test/java/org/jongo/spike/projection/JacksonProjectionTest.java
@@ -19,7 +19,10 @@ package org.jongo.spike.projection;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.DBObject;
+import org.jongo.marshall.jackson.bson4jackson.BsonModule;
+import org.jongo.marshall.jackson.bson4jackson.MongoBsonFactory;
 import org.jongo.marshall.jackson.configuration.Mapping;
+import org.jongo.marshall.jackson.configuration.PropertyModifier;
 import org.jongo.model.Coordinate;
 import org.jongo.model.Fox;
 import org.jongo.model.Friend;
@@ -31,11 +34,14 @@ import static org.fest.assertions.Assertions.assertThat;
 public class JacksonProjectionTest {
 
     private JacksonProjection projection;
-    private ObjectMapper mapper;
 
     @Before
     public void setUp() throws Exception {
-        mapper = Mapping.defaultMapping().getMapper();
+        ObjectMapper mapper = new ObjectMapper(MongoBsonFactory.createFactory());
+        Mapping.Builder builder = new Mapping.Builder(mapper);
+        builder.registerModule(new BsonModule());
+        builder.addModifier(new PropertyModifier());
+        Mapping mapping = builder.build();
         projection = new JacksonProjection(mapper);
     }
 
