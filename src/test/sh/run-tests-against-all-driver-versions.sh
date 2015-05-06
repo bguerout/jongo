31b7345f40957cca226bdbd6fe16d6f337697d6e
Benoit Guerout <bguerout@gmail.com>
2015-05-06 19:24:40 +0200

    Polish test script

--- src/test/sh/run-tests-against-all-driver-versions.sh
+++ src/test/sh/run-tests-against-all-driver-versions.sh
@@ -8,36 +8,36 @@ EXCLUDED_VERSIONS="2.13.0-rc0 3.0.0-test-SNAPSHOT"
 A_VERSION_HAS_FAILED=false
 OPTS=$*
 
-echo "Executing tests with mongo-java-driver[$MINIMAL_VERSION+] dependencies available on Nexus http://repository.sonatype.org"
-
 mkdir -p "$OUTPUT_DIR";
 DB_VERSIONS=( "2.6" "3.0" )
-VERSIONS=$(curl -so "$MONGO_ARTIFACTS_FILE" "$NEXUS_URL" &&  grep -e "version" "$MONGO_ARTIFACTS_FILE" | sed 's/<version>//g' | sed 's/<\/version>//g' | tr -s " " | sort | uniq);
+DRIVER_VERSIONS=$(curl -so "$MONGO_ARTIFACTS_FILE" "$NEXUS_URL" &&  grep -e "version" "$MONGO_ARTIFACTS_FILE" | sed 's/<version>//g' | sed 's/<\/version>//g' | tr -s " " | sort | uniq);
+
+echo "Executing tests for mongo-java-driver[$DRIVER_VERSIONS] dependencies available on Nexus http://repository.sonatype.org"
 
 for db_version in $DB_VERSIONS
 do
-    for version in $VERSIONS
+    for driver_version in $DRIVER_VERSIONS
     do
-        CURRENT=$(echo "$version" | sed "s/\.//g" | sed "s/-.*//g")
+        CURRENT=$(echo "$driver_version" | sed "s/\.//g" | sed "s/-.*//g")
         MINIMAL=$(echo "$MINIMAL_VERSION" | sed "s/\.//g" | sed "s/-.*//g")
 
-        if [ ${CURRENT:0:1} -gt ${MINIMAL:0:1} ] || [ ${CURRENT:0:1} -eq ${MINIMAL:0:1} -a ${CURRENT:1} -ge ${MINIMAL:1} ] && [[ $EXCLUDED_VERSIONS != *"$version"* ]];
+        if [ ${CURRENT:0:1} -gt ${MINIMAL:0:1} ] || [ ${CURRENT:0:1} -eq ${MINIMAL:0:1} -a ${CURRENT:1} -ge ${MINIMAL:1} ] && [[ $EXCLUDED_VERSIONS != *"$driver_version"* ]];
         then
 
-          echo "Running tests against MongoDB ${db_version} and java driver ${version}"
+          echo "Running tests against java driver ${driver_version} and MongoDB ${db_version}"
           mvn verify $OPTS \
             -Djongo.test.db.version="${db_version}" \
-            -Dmongo.version="$version" \
+            -Dmongo.version="$driver_version" \
             -DreportFormat=plain \
             -DuseFile=false \
             -l $OUTPUT_DIR/build-"$version".log
 
           if [ "$?" -ne "0" ];
           then
-            echo "$version FAILED, please check file $OUTPUT_DIR/build-$version.log"
+            echo "${driver_version} with db ${db_version} FAILED, please check file $OUTPUT_DIR/build-$version.log"
             A_VERSION_HAS_FAILED=true;
           else
-            echo "$version SUCCESS"
+            echo "${driver_version} SUCCESS"
           fi
         fi
     done
